126cba4c4a7ae2bc1f898f031e3c81fe41fe0bc1
Alisson Vieira <alisson.v3@hotmail.com>
2021-04-15 19:39:29 -0300

    Tela de Login parcialmente finalizada

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
-rectangle = require("rectangle")
+local suit = require 'suit'
+local utf8 = require 'utf8'
 
 local ACTUAL_SCREEN = "LOGGIN_SCREEN"
 local button_x = 75
@@ -7,27 +8,77 @@ local button_width = 250
 local button_height = 50
 local border_radius = 10
 
-function love.draw()
+local x = 75
+local y = 200
+local w = 250
+local h = 50
+local border_radius = 10
 
-    if ACTUAL_SCREEN == "LOGGIN_SCREEN" then
-        -- Cores de fundo
-        red = 192/255
-        green = 195/255
-        blue = 195/255
+local red = 192/255
+local green = 195/255
+local blue = 195/255
+
+local input1 = {text = ""}
+local input2 = {text = ""}
+local button = {}
+
+local function validate(input)
+    local len = utf8.len(input.text)
+
+    input.cursor = input.cursor - (len - utf8.len(input.text))
+end
+
+function love.update(dt)
+    love.graphics.setColor(red, green, blue)
+    suit.Input(input1, x, y, w, h)
+    suit.Input(input2, x, y+100, w, h)
+
     
-        love.graphics.setBackgroundColor( red, green, blue, 0)
+    --suit.Button(button, x, y+200, w, h)
+
+    validate(input1)
+    validate(input2)
+
+end
+
+function love.draw(dt)
+    love.graphics.setBackgroundColor(red, green, blue, 0)
+    suit.draw()
+
+    love.graphics.setColor(0, 255, 0)
+    --suit.Button(button, x, y+200, w, h)
+    love.graphics.rectangle("fill", x, y+200, w, h, border_radius)
+    --[[
+        -- Cores de fundo
+
+    
+        
         -- to do:
         -- ## limg: shinoPet
         
         rectangle:draw()
 
         -- ## img: cadastre-se
-    end
+    ]]
+end
 
+function love.textinput(t)
+    suit.textinput(t)
+end
+
+function love.keypressed(key)
+    suit.keypressed(key)
+end
+
+-- if you want IME input, requires a font that can display the characters
+function love.textedited(text, start, length)
+    suit.textedited(text, start, length)
 end
 
 function love.mousepressed(x, y)
     if x >= button_x and x <= button_x + button_width and y >= button_y and y <= button_y + button_height then
         print("voce clicou no botao")
+        print("login        ", input1["text"])
+        print("senha        ", input2["text"])
     end
 end
